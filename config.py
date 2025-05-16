f0862753e50ee67c8c75e0d43dcafae947cb5307
ScalperLeal <lealright@gmail.com>
2025-05-15 22:15:07 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 # Credenciais da Binance
-API_KEY = os.getenv (ndiz4nLHmsvNLhmyHBvS3O5XJUIQ3B76r5Xyur8dc5gKG9eo9GV7RCAPN3ASzbu0)
-API_SECRET = os.getenv (dfzv8066w1FjXGqPbEyeb0RhyMEKL3DJh8xVd1G4ZfXfXuZ85FztrqdyRVYtrY71)
+API_KEY = os.getenv ("API_KEY")
+API_SECRET = os.getenv ("API_SECRET")
 
 # Configurações do mercado
 SYMBOL = "BTCUSDT"
@@ -14,5 +14,5 @@ RISK_PER_TRADE_PCT = 0.5
 MAX_DRAWDOWN_PCT = 5
 
 # Telegram
-TELEGRAM_BOT_TOKEN = os.getenv ("7228891982:AAE8i0SwYeEVJFo8o02avyeTgtn4nHeoTQI")
-TELEGRAM_CHAT_ID = os.getenv ("858755726")
+TELEGRAM_BOT_TOKEN = os.getenv ("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv ("TELEGRAM_CHAT_ID")
